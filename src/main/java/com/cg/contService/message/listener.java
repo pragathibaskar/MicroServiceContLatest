bd5bc43f0e88496b617bc9859f641a148c96c0c5
pragathibaskar <47776430+pragathibaskar@users.noreply.github.com>
2019-09-26 22:05:00 +0530

    Update listener.java

--- src/main/java/com/cg/contService/message/listener.java
+++ src/main/java/com/cg/contService/message/listener.java
@@ -1,5 +1,10 @@
 package com.cg.contService.message;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.transaction.annotation.Transactional;
 import com.cg.contService.domain.Cert;
+import java.io.IOException;
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonMappingException;
